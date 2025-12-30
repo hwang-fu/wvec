296f05261b86274849e00d918cfcd8133cb5a6cb
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-30 11:41:48 +0100

    feat: add FFI smoke test function in fortran

--- fortran/wvec_types.f90
+++ fortran/wvec_types.f90
@@ -4,4 +4,13 @@ module wvec_types
   implicit none
 
   ! TODO: define model params, array types, etc.
+
+contains
+
+  function wvec_add_smoke_test(a, b) result(c) bind(C, name="wvec_add_smoke_test")
+    integer(c_int), intent(in), value :: a, b
+    integer(c_int) :: c
+    c = a + b
+  end function wvec_add_smoke_test
+
 end module wvec_types
