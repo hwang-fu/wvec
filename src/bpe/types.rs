96e662088ac95ed05fbd9a3eefe7fce760dff64f
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-30 17:57:58 +0100

    feat: add special token constants

--- src/bpe/types.rs
+++ src/bpe/types.rs
@@ -2,3 +2,9 @@
 
 /// A token ID (index into vocabulary)
 pub type TokenId = u32;
+
+/// Special token constants
+pub const UNK_TOKEN: &str = "[UNK]";
+pub const PAD_TOKEN: &str = "[PAD]";
+pub const BOS_TOKEN: &str = "[BOS]";
+pub const EOS_TOKEN: &str = "[EOS]";
