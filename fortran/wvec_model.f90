7d593e3c7c6ed93ef048220b60b89b77e7dc416a
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-31 01:35:29 +0100

    feat: add W2VModel

--- fortran/wvec_model.f90
+++ fortran/wvec_model.f90
@@ -4,6 +4,15 @@ module wvec_model
   use wvec_types
   implicit none
 
-  ! TODO: model data structure, embedding matrices
+  !> Word2Vec model with input and output embeddings
+  !> Matrices are (dim, vocab_size) - each column is one word's vector
+  type :: W2VModel
+    integer(c_int) :: vocab_size = 0
+    integer(c_int) :: dim = 0
+    real(c_float), allocatable :: w_in(:, :)   ! Input embeddings (dim, vocab_size)
+    real(c_float), allocatable :: w_out(:, :)  ! Output embeddings (dim, vocab_size)
+  end type W2VModel
+
+contains
 
 end module wvec_model
