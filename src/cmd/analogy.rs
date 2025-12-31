8dcedf4dde62bdca875bfcdac078a9ea3490941b
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-31 17:26:30 +0100

    feat: add cosine_similarity()

--- src/cmd/analogy.rs
+++ src/cmd/analogy.rs
@@ -1,4 +1,15 @@
 
+fn cosine_similarity(a: &[f32], b: &[f32]) -> f32 {
+    let dot: f32 = a.iter().zip(b.iter()).map(|(x, y)| x * y).sum();
+    let norm_a: f32 = a.iter().map(|x| x * x).sum::<f32>().sqrt();
+    let norm_b: f32 = b.iter().map(|x| x * x).sum::<f32>().sqrt();
+
+    if norm_a > 0.0 && norm_b > 0.0 {
+        dot / (norm_a * norm_b)
+    } else {
+        0.0
+    }
+}
 fn print_help() {
     println!(
         "wvec analogy - Solve word analogies
