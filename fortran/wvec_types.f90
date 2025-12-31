fa9be83531553a975ec6caa713c0297ee125e58c
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-31 01:09:07 +0100

    feat: add wvec_array_sum()

--- fortran/wvec_types.f90
+++ fortran/wvec_types.f90
@@ -3,8 +3,6 @@ module wvec_types
   use, intrinsic :: iso_c_binding
   implicit none
 
-  ! TODO: define model params, array types, etc.
-
 contains
 
   function wvec_add_smoke_test(a, b) result(c) bind(C, name="wvec_add_smoke_test")
@@ -12,5 +10,18 @@ contains
     integer(c_int) :: c
     c = a + b
   end function wvec_add_smoke_test
+
+  !> Computes sum of a float array (FFI array passing test)
+  function wvec_array_sum(arr, n) result(total) bind(C, name="wvec_array_sum")
+    integer(c_int), intent(in), value :: n
+    real(c_float), intent(in) :: arr(n)
+    real(c_float) :: total
+    integer :: i
+
+    total = 0.0
+    do i = 1, n
+      total = total + arr(i)
+    end do
+  end function wvec_array_sum
 
 end module wvec_types
