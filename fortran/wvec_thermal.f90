155c861ff0c36520ac4e099bfacf4f94587dab5a
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-31 16:56:27 +0100

    feat: add decls

--- fortran/wvec_thermal.f90
+++ fortran/wvec_thermal.f90
@@ -3,6 +3,13 @@ module wvec_thermal
   use, intrinsic :: iso_c_binding
   implicit none
 
-  ! TODO: read /sys/class/thermal/, throttle on overheat
+  !> Default thermal zone path (x86_pkg_temp is reliable for Intel CPUs)
+  character(len=*), parameter :: DEFAULT_THERMAL_PATH = &
+                                 "/sys/class/thermal/thermal_zone10/temp"
+
+  !> Default temperature threshold in Celsius
+  integer, parameter :: DEFAULT_THRESHOLD_C = 85
+
+contains
 
 end module wvec_thermal
