51137ae22caa8eccabb0abe36f5a65272535a565
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-31 01:33:06 +0100

    feat: add sdot()

--- fortran/wvec_blas.f90
+++ fortran/wvec_blas.f90
@@ -1,8 +1,16 @@
-! wvec_blas.f90 - BLAS wrappers for linear algebra operations
+! wvec_blas.f90 - BLAS interfaces for linear algebra operations
 module wvec_blas
   use, intrinsic :: iso_c_binding
   implicit none
 
-  ! TODO: wrappers for sdot, saxpy, snrm2, sscal, sgemm
+  ! BLAS Level 1 interfaces (vector operations)
+  interface
+    !> Dot product: result = x · y
+    real(c_float) function sdot(n, x, incx, y, incy)
+      import :: c_float, c_int
+      integer(c_int), intent(in), value :: n, incx, incy
+      real(c_float), intent(in) :: x(*), y(*)
+    end function sdot
+  end interface
 
 end module wvec_blas
