dcf8c5768f86d020ed6760723f9b656b01b9f17a
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-31 16:56:42 +0100

    feat: add wvec_thermal_read()

--- fortran/wvec_thermal.f90
+++ fortran/wvec_thermal.f90
@@ -12,4 +12,48 @@ module wvec_thermal
 
 contains
 
+  !> Read CPU temperature from sysfs
+  !> Parameters:
+  !>   path: path to thermal zone temp file (C string)
+  !>   path_len: length of path string
+  !>   temp_mc: output temperature in millidegrees Celsius
+  !> Returns: 0 on success, -4 on file I/O error
+  function wvec_thermal_read(path, path_len, temp_mc) &
+    result(status) bind(C, name="wvec_thermal_read")
+    character(kind=c_char), intent(in) :: path(*)
+    integer(c_int), intent(in), value :: path_len
+    integer(c_int), intent(out) :: temp_mc
+    integer(c_int) :: status
+
+    character(len=:), allocatable :: fpath
+    integer :: unit_num, ios, i
+
+    ! Convert C string to Fortran string
+    allocate (character(len=path_len) :: fpath)
+    do i = 1, path_len
+      fpath(i:i) = path(i)
+    end do
+
+    ! Open and read temperature file
+    open (newunit=unit_num, file=fpath, status='old', action='read', iostat=ios)
+    if (ios /= 0) then
+      deallocate (fpath)
+      temp_mc = 0
+      status = -4  ! ERR_FILE_IO
+      return
+    end if
+
+    read (unit_num, *, iostat=ios) temp_mc
+    close (unit_num)
+    deallocate (fpath)
+
+    if (ios /= 0) then
+      temp_mc = 0
+      status = -4  ! ERR_FILE_IO
+      return
+    end if
+
+    status = 0  ! SUCCESS
+  end function wvec_thermal_read
+
 end module wvec_thermal
